6883cd4514a25cfb0afcb2a9e8939edf618f7d8b
l1aF-2027 <22520460@gm.uit.edu.vn>
2025-02-14 15:24:31 +0700

    change next config and nextjs.yml

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   experimental: {
     runtime: "edge",
   },
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/CV-Website",
+  assetPrefix: "/CV-Website",
 };
 
 export default nextConfig;
