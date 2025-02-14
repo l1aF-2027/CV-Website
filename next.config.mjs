63218d45e580d01afebb2532fb4149e442c00605
l1aF-2027 <22520460@gm.uit.edu.vn>
2025-02-14 15:44:43 +0700

    change next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    runtime: "edge",
-  },
-
-  basePath: "/CV-Website",
-  assetPrefix: "/CV-Website/public",
 };
 
 export default nextConfig;
