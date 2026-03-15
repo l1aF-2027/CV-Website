26f435555d12aa87c898d5f5dea4c29fbcc46a33
l1aF-2027 <22520460@gm.uit.edu.vn>
2026-03-15 09:02:27 +0700

    change to reduce LCP time

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    formats: ['image/avif', 'image/webp'],
+    formats: ["image/avif", "image/webp"],
     deviceSizes: [320, 420, 768, 1024, 1280, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
+  swcMinify: true,
+  compress: true,
+  productionBrowserSourceMaps: false,
 };
 
 export default nextConfig;
